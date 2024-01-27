5d8d7e5b194cf3fbe0df62b23f150d6df7047ba4
itseskimo <ayushmatmotgill@gmail.com>
2024-01-27 18:04:19 +0530

    update:add svg

--- src/config/data.tsx
+++ src/config/data.tsx
@@ -16,6 +16,11 @@ preserveAspectRatio="xMidYMid meet">
 </g>
 </svg>`
 ,
+`  <svg xmlns="http://www.w3.org/2000/svg" width="22.666" height="25.093" viewBox="0 0 37.666 22.093">
+<path id="Password-Icon-SVG-lskfnrsdsd" d="M32.83,1.54A20.927,20.927,0,0,0,14,12.589,20.927,20.927,0,0,0,32.83,23.633,20.927,20.927,0,0,0,51.662,12.589,20.927,20.927,0,0,0,32.83,1.54m0,16.185a5.139,5.139,0,1,1,5.136-5.136,5.139,5.139,0,0,1-5.136,5.136" transform="translate(-13.996 -1.54)"/>
+</svg>
+`
+,
 ` <svg  xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 36 36" fill="none">
 
 <g clipPath="url(#clip0_1605_10756)">
