e7d553f3d0c5405da27fe4efca036c409385323e
itseskimo <ayushmatmotgill@gmail.com>
2024-01-28 00:21:13 +0530

    update:remove deployment errors

--- src/sections/PhysioView/PhysioTable/PhysioTable.jsx
+++ src/sections/PhysioView/PhysioTable/PhysioTable.jsx
@@ -5,158 +5,158 @@ import { useNavigate } from 'react-router-dom';
 
 const PhysioTable = () => {
 
-    // const dispatch = useDispatch();
-    // const { bookedSlots, selectedRemarks } = useSelector((state) => state.doctor);
-    // const [selectedDates, setSelectedDates] = useState([]);
-    // const [clientId, setClientId] = useState('');
-    // const [token, setToken] = useState('');
-    // const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const { bookedSlots, selectedRemarks } = useSelector((state) => state.doctor);
+    const [selectedDates, setSelectedDates] = useState([]);
+    const [clientId, setClientId] = useState('');
+    const [token, setToken] = useState('');
+    const navigate = useNavigate();
 
-    // useEffect(() => {
-    //     if (localStorage.getItem("userInfo")) {
-    //         const data = localStorage.getItem("userInfo");
-    //         if (data) {
-    //             const loginData = JSON.parse(data);
-    //             setClientId(loginData.name);
-    //             setToken(loginData.token);
-    //             dispatch(getPhysioCalendar({ token: loginData.token }))
-    //         }
-    //     }
-    // }, []);
-
-
-
-
-    // useEffect(() => {
-    //     setSelectedDates((bookedSlots && bookedSlots[0]?.calendars) ?? []);
-    // }, [bookedSlots]);
-
-    // function handleSubmit() {
-    //     const physioData = selectedDates;
-    //     dispatch(addPhysioCalendar({ physioData, token }));
-    // }
-
-    // function handleClick(day, date, selectedSlot) {
-    //     if (currentDayIndex !== 0) {
-    //         alert(`Slots available for Physios can only be selected on Sunday's`);
-    //         return;
-    //     }
-
-    //     setSelectedDates((prevDates) => {
-    //         const existingDateIndex = prevDates.findIndex((dateObj) => dateObj.date === date);
-
-    //         if (existingDateIndex !== -1) {
-    //             // Date already exists, toggle selectedSlot
-    //             return prevDates.map((dateObj, index) => {
-    //                 if (index === existingDateIndex) {
-    //                     // Update the existing date
-    //                     const existingSlots = dateObj.selectedSlots;
-    //                     const existingSlotIndex = existingSlots.findIndex((slot) => slot.timestamp === selectedSlot.timestamp);
-
-    //                     if (existingSlotIndex !== -1) {
-    //                         // SelectedSlot already exists, remove it
-    //                         return {
-    //                             ...dateObj,
-    //                             selectedSlots: [
-    //                                 ...existingSlots.slice(0, existingSlotIndex),
-    //                                 ...existingSlots.slice(existingSlotIndex + 1),
-    //                             ],
-    //                         };
-    //                     } else {
-    //                         // SelectedSlot doesn't exist, add it
-    //                         return { ...dateObj, selectedSlots: [...existingSlots, selectedSlot] };
-    //                     }
-    //                 }
-    //                 return dateObj;
-    //             });
-    //         } else {
-    //             // Date doesn't exist, create a new object with a unique selectedSlot
-    //             return [...prevDates, { day, date, selectedSlots: [selectedSlot] }];
-    //         }
-    //     });
-    // }
+    useEffect(() => {
+        if (localStorage.getItem("userInfo")) {
+            const data = localStorage.getItem("userInfo");
+            if (data) {
+                const loginData = JSON.parse(data);
+                setClientId(loginData.name);
+                setToken(loginData.token);
+                dispatch(getPhysioCalendar({ token: loginData.token }))
+            }
+        }
+    }, []);
 
 
 
-    // const generateTimeSlots = () => {
-    //     const startTime = new Date('2024-01-01T09:00:00');
-    //     const endTime = new Date('2024-01-01T20:00:00');
-    //     const timeSlots = [];
 
-    //     let currentTime = new Date(startTime);
+    useEffect(() => {
+        setSelectedDates((bookedSlots && bookedSlots[0]?.calendars) ?? []);
+    }, [bookedSlots]);
 
-    //     while (currentTime <= endTime) {
-    //         let period = 'morning';
+    function handleSubmit() {
+        const physioData = selectedDates;
+        dispatch(addPhysioCalendar({ physioData, token }));
+    }
 
-    //         const hour = currentTime.getHours();
-    //         if (hour >= 12 && hour < 17) {
-    //             period = 'afternoon';
-    //         } else if (hour >= 17) {
-    //             period = 'evening';
-    //         }
+    function handleClick(day, date, selectedSlot) {
+        if (currentDayIndex !== 0) {
+            alert(`Slots available for Physios can only be selected on Sunday's`);
+            return;
+        }
 
-    //         timeSlots.push({
-    //             timestamp: currentTime.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' }),
-    //             assignedDoctor: '',
-    //             remark: '',
-    //             period: period,
-    //         });
+        setSelectedDates((prevDates) => {
+            const existingDateIndex = prevDates.findIndex((dateObj) => dateObj.date === date);
 
-    //         currentTime.setMinutes(currentTime.getMinutes() + 45);
-    //     }
+            if (existingDateIndex !== -1) {
+                // Date already exists, toggle selectedSlot
+                return prevDates.map((dateObj, index) => {
+                    if (index === existingDateIndex) {
+                        // Update the existing date
+                        const existingSlots = dateObj.selectedSlots;
+                        const existingSlotIndex = existingSlots.findIndex((slot) => slot.timestamp === selectedSlot.timestamp);
 
-    //     return timeSlots;
-    // };
-
-
-
-    // const currentDayIndex = new Date().getDay();
-
-    // const getFormattedDate = (offset) => {
-    //     const today = new Date();
-    //     const targetDate = new Date(today);
-    //     targetDate.setDate(today.getDate() + offset);
-
-    //     const month = new Intl.DateTimeFormat('en-US', { month: 'short' }).format(targetDate);
-    //     const date = targetDate.getDate();
-
-    //     return `${month} ${date}`;
-    // };
-
-    // let calendarController = [
-    //     { day: 'Saturday', date: getFormattedDate(0), slots: generateTimeSlots() },
-    //     { day: 'Monday', date: getFormattedDate(2), slots: generateTimeSlots() },
-    //     { day: 'Tuesday', date: getFormattedDate(3), slots: generateTimeSlots() },
-    //     { day: 'Wednesday', date: getFormattedDate(4), slots: generateTimeSlots() },
-    //     { day: 'Thursday', date: getFormattedDate(5), slots: generateTimeSlots() },
-    //     { day: 'Friday', date: getFormattedDate(6), slots: generateTimeSlots() },
-    //     { day: 'Saturday', date: getFormattedDate(0), slots: generateTimeSlots() },
-    // ];
-
-    // // Include the entire week starting from Monday if it's Sunday
-    // if (currentDayIndex === 0) {
-    //     calendarController = [
-    //         { day: 'Monday', date: getFormattedDate(2), slots: generateTimeSlots() },
-    //         { day: 'Tuesday', date: getFormattedDate(3), slots: generateTimeSlots() },
-    //         { day: 'Wednesday', date: getFormattedDate(4), slots: generateTimeSlots() },
-    //         { day: 'Thursday', date: getFormattedDate(5), slots: generateTimeSlots() },
-    //         { day: 'Friday', date: getFormattedDate(6), slots: generateTimeSlots() },
-    //         { day: 'Saturday', date: getFormattedDate(7), slots: generateTimeSlots() }
-    //     ]
-    // } else {
-    //     // Filter days before the current day
-    //     calendarController = calendarController.filter((_, index) => index >= currentDayIndex);
-    // }
-
-    // // Now, calendarController contains the desired calendar information
+                        if (existingSlotIndex !== -1) {
+                            // SelectedSlot already exists, remove it
+                            return {
+                                ...dateObj,
+                                selectedSlots: [
+                                    ...existingSlots.slice(0, existingSlotIndex),
+                                    ...existingSlots.slice(existingSlotIndex + 1),
+                                ],
+                            };
+                        } else {
+                            // SelectedSlot doesn't exist, add it
+                            return { ...dateObj, selectedSlots: [...existingSlots, selectedSlot] };
+                        }
+                    }
+                    return dateObj;
+                });
+            } else {
+                // Date doesn't exist, create a new object with a unique selectedSlot
+                return [...prevDates, { day, date, selectedSlots: [selectedSlot] }];
+            }
+        });
+    }
 
 
-    // console.log(currentDayIndex)
+
+    const generateTimeSlots = () => {
+        const startTime = new Date('2024-01-01T09:00:00');
+        const endTime = new Date('2024-01-01T20:00:00');
+        const timeSlots = [];
+
+        let currentTime = new Date(startTime);
+
+        while (currentTime <= endTime) {
+            let period = 'morning';
+
+            const hour = currentTime.getHours();
+            if (hour >= 12 && hour < 17) {
+                period = 'afternoon';
+            } else if (hour >= 17) {
+                period = 'evening';
+            }
+
+            timeSlots.push({
+                timestamp: currentTime.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' }),
+                assignedDoctor: '',
+                remark: '',
+                period: period,
+            });
+
+            currentTime.setMinutes(currentTime.getMinutes() + 45);
+        }
+
+        return timeSlots;
+    };
+
+
+
+    const currentDayIndex = new Date().getDay();
+
+    const getFormattedDate = (offset) => {
+        const today = new Date();
+        const targetDate = new Date(today);
+        targetDate.setDate(today.getDate() + offset);
+
+        const month = new Intl.DateTimeFormat('en-US', { month: 'short' }).format(targetDate);
+        const date = targetDate.getDate();
+
+        return `${month} ${date}`;
+    };
+
+    let calendarController = [
+        { day: 'Saturday', date: getFormattedDate(0), slots: generateTimeSlots() },
+        { day: 'Monday', date: getFormattedDate(2), slots: generateTimeSlots() },
+        { day: 'Tuesday', date: getFormattedDate(3), slots: generateTimeSlots() },
+        { day: 'Wednesday', date: getFormattedDate(4), slots: generateTimeSlots() },
+        { day: 'Thursday', date: getFormattedDate(5), slots: generateTimeSlots() },
+        { day: 'Friday', date: getFormattedDate(6), slots: generateTimeSlots() },
+        { day: 'Saturday', date: getFormattedDate(0), slots: generateTimeSlots() },
+    ];
+
+    // Include the entire week starting from Monday if it's Sunday
+    if (currentDayIndex === 0) {
+        calendarController = [
+            { day: 'Monday', date: getFormattedDate(2), slots: generateTimeSlots() },
+            { day: 'Tuesday', date: getFormattedDate(3), slots: generateTimeSlots() },
+            { day: 'Wednesday', date: getFormattedDate(4), slots: generateTimeSlots() },
+            { day: 'Thursday', date: getFormattedDate(5), slots: generateTimeSlots() },
+            { day: 'Friday', date: getFormattedDate(6), slots: generateTimeSlots() },
+            { day: 'Saturday', date: getFormattedDate(7), slots: generateTimeSlots() }
+        ]
+    } else {
+        // Filter days before the current day
+        calendarController = calendarController.filter((_, index) => index >= currentDayIndex);
+    }
+
+    // Now, calendarController contains the desired calendar information
+
+
+    console.log(currentDayIndex)
 
     return (
         <section className='flex flex-col items-start p-6'>
 
-            {/* <div className='md:mb-5 hidden md:flex items-center gap-6 justify-between  w-full '>
+            <div className='md:mb-5 hidden md:flex items-center gap-6 justify-between  w-full '>
                 <span className=' text-white text-xl'>Hello {clientId}</span>
                 <span onClick={() => [localStorage.clear('userInfo'), navigate('/')]} className='text-white'>Logout</span>
             </div>
@@ -199,7 +199,7 @@ const PhysioTable = () => {
                 className='bg-[#081c1f] px-10 py-2 text-white  rounded-md shadow-sm shadow-[#00acc1] font-light'
             >
                 SAVE
-            </button> */}
+            </button>
         </section>
     );
 };
