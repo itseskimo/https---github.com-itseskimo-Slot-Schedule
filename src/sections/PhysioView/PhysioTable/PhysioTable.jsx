f2badb2cfd493ed41fb56998a6c6b635cb515a21
itseskimo <ayushmatmotgill@gmail.com>
2024-01-27 07:39:02 +0530

    update:responsive

--- src/sections/PhysioView/PhysioTable/PhysioTable.jsx
+++ src/sections/PhysioView/PhysioTable/PhysioTable.jsx
@@ -161,7 +161,7 @@ const PhysioTable = () => {
                 <span onClick={() => [localStorage.clear('userInfo'), navigate('/')]} className='text-white'>Logout</span>
             </div>
             
-            <div className='md:border-t-[1px] border-[#FFFFFF80] border-solid w-full mb-6'></div>
+            <div className='md:border-t-[1px] border-[#FFFFFF80] border-solid w-full md:mb-6'></div>
             <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-8'>
                 {calendarController.map((item, index) => (
                     <div key={index} className='flex flex-col text-white gap-3'>
