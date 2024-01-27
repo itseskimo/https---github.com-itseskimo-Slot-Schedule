43b7970feaaa4a9636c89dbb04def805c3b84521
itseskimo <ayushmatmotgill@gmail.com>
2024-01-27 17:54:47 +0530

    update:responsiveness

--- src/sections/OperationsView/OperationsTable/OperationsTable.jsx
+++ src/sections/OperationsView/OperationsTable/OperationsTable.jsx
@@ -209,7 +209,7 @@ const OperationsTable = () => {
                 </div>
                 <span onClick={() => [localStorage.clear('userInfo'), navigate('/')]} className='text-white'>Logout</span>
             </div>
-            <div className='border-t-[1px] border-[#FFFFFF80] border-solid w-full mb-6'></div>
+            <div className='md:border-t-[1px] border-[#FFFFFF80] border-solid w-full md:mb-6'></div>
             <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-8'>
                 {calendarController.map((item, index) => {
                     const filteredSlots = selectedPeriod
